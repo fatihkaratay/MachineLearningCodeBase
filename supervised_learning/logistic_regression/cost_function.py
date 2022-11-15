9bebe11a0ad02eeafccdfe7e4e6463405fea2b38
Fatih Karatay <fatihkaratay@gmail.com>
2022-11-15 11:14:52 -0500

    regularized cost function is added

--- supervised_learning/logistic_regression/cost_function.py
+++ supervised_learning/logistic_regression/cost_function.py
@@ -29,3 +29,35 @@ def compute_cost(X, y, w, b, lambda_=1):
     total_cost = total_cost / m
 
     return total_cost
+
+
+def compute_cost_reg(X, y, w, b, lambda_=1):
+    """
+    Cost function for regularized logistic regression
+    Computes the cost over all examples
+    Args:
+      X : (array_like Shape (m,n)) data, m examples by n features
+      y : (array_like Shape (m,)) target value
+      w : (array_like Shape (n,)) Values of parameters of the model
+      b : (array_like Shape (n,)) Values of bias parameter of the model
+      lambda_ : (scalar, float)    Controls amount of regularization
+    Returns:
+      total_cost: (scalar)         cost
+    """
+
+    m, n = X.shape
+
+    # Calls the compute_cost function that you implemented above
+    cost_without_reg = compute_cost(X, y, w, b)
+
+    # You need to calculate this value
+    reg_cost = 0.
+
+    reg_cost = 0
+    for j in range(n):
+        reg_cost += (w[j] ** 2)
+
+    # Add the regularization cost to get the total cost
+    total_cost = cost_without_reg + (lambda_ / (2 * m)) * reg_cost
+
+    return total_cost
