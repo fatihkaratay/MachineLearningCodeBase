9854738f9ea1392ae5ccd5bcee0dcf524a43bc0e
Fatih Karatay <fatihkaratay@gmail.com>
2022-11-15 10:49:08 -0500

    computing the cost function

---
+++ supervised_learning/linear_resgression/cost_function.py
@@ -0,0 +1,31 @@
+"""
+Calculating the cost function in linear regression
+"""
+
+
+def compute_cost(x, y, w, b):
+    """
+    Computes the cost function for linear regression.
+
+    Args:
+        x (ndarray): Shape (m,) Input to the model (Population of cities)
+        y (ndarray): Shape (m,) Label (Actual profits for the cities)
+        w, b (scalar): Parameters of the model
+
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression
+               to fit the data points in x and y
+    """
+    # number of training examples
+    m = x.shape[0]
+
+    total_cost = 0
+
+    for i in range(m):
+        f_wb = w * x[i] + b
+        cost = (f_wb - y[i]) ** 2
+        total_cost = total_cost + cost
+
+    total_cost = (1 / (2 * m)) * total_cost
+
+    return total_cost
