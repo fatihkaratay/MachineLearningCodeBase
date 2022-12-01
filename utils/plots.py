3a4ca084e6273b2e8de416babb170448b9dc9b6a
Fatih Karatay <fatihkaratay@gmail.com>
2022-12-01 15:25:44 -0500

    plotting the fine-tuning regularization

--- utils/plots.py
+++ utils/plots.py
@@ -70,3 +70,42 @@ def plt_optimal_degree(X_train, y_train, X_cv, y_cv, x, y_pred, x_ideal, y_ideal
     plt.tight_layout()
 
     plt.show()
+
+
+def plt_tune_regularization(X_train, y_train, X_cv, y_cv, x, y_pred, err_train, err_cv, optimal_reg_idx, lambda_range):
+    fig, ax = plt.subplots(1,2,figsize=(8,4))
+    fig.canvas.toolbar_visible = False
+    fig.canvas.header_visible = False
+    fig.canvas.footer_visible = False
+
+    ax[0].set_title("predictions vs data", fontsize = 12)
+    ax[0].set_xlabel("x")
+    ax[0].set_ylabel("y")
+
+    ax[0].scatter(X_train, y_train, color="red",           label="train")
+    ax[0].scatter(X_cv, y_cv,       color=dlc["dlorange"], label="cv")
+    ax[0].set_xlim(ax[0].get_xlim())
+    ax[0].set_ylim(ax[0].get_ylim())
+#   ax[0].plot(x, y_pred[:,:],  lw=0.5, label=[f"$\lambda =${i}" for i in lambda_range])
+    for i in (0, 3, 7, 9):
+        ax[0].plot(x, y_pred[:, i],  lw=0.5, label=f"$\lambda =${lambda_range[i]}")
+    ax[0].legend()
+
+    ax[1].set_title("error vs regularization", fontsize=12)
+    ax[1].plot(lambda_range, err_train[:], label="train error", color=dlc["dlblue"])
+    ax[1].plot(lambda_range, err_cv[:],    label="cv error",    color=dlc["dlorange"])
+    ax[1].set_xscale('log')
+    ax[1].set_ylim(*ax[1].get_ylim())
+    opt_x = lambda_range[optimal_reg_idx]
+    ax[1].vlines(opt_x, *ax[1].get_ylim(), color = "black", lw=1)
+    ax[1].annotate("optimal lambda", (opt_x,150000), xytext=(-80, 10), textcoords="offset points",
+                  arrowprops={'arrowstyle': 'simple'})
+    ax[1].set_xlabel("regularization (lambda)")
+    ax[1].set_ylabel("error")
+    fig.suptitle("Tuning Regularization", fontsize = 12)
+    ax[1].text(0.05, 0.44, "High\nVariance", fontsize=12, ha='left', transform=ax[1].transAxes, color=dlc["dlblue"])
+    ax[1].text(0.95, 0.44, "High\nBias",    fontsize=12, ha='right', transform=ax[1].transAxes, color=dlc["dlblue"])
+    ax[1].legend(loc='upper left')
+    plt.tight_layout()
+    plt.show()
+
