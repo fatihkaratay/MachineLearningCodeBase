c810ef78c060d58e35352262f507229b6a5cf4f2
Fatih Karatay <fatihkaratay@gmail.com>
2022-12-01 15:10:13 -0500

    plotting optimal degree function is added

--- utils/plots.py
+++ utils/plots.py
@@ -32,3 +32,41 @@ def plt_train_test(X_train, y_train, X_test, y_test, x, y_pred, x_ideal, y_ideal
     ax.legend(loc='upper left')
     plt.tight_layout()
     plt.show()
+
+
+def plt_optimal_degree(X_train, y_train, X_cv, y_cv, x, y_pred, x_ideal, y_ideal, err_train, err_cv, optimal_degree, max_degree):
+    fig, ax = plt.subplots(1,2,figsize=(8,4))
+    fig.canvas.toolbar_visible = False
+    fig.canvas.header_visible = False
+    fig.canvas.footer_visible = False
+
+    ax[0].set_title("predictions vs data",fontsize = 12)
+    ax[0].set_xlabel("x")
+    ax[0].set_ylabel("y")
+
+    ax[0].plot(x_ideal, y_ideal, "--", color="orangered", label="y_ideal", lw=1)
+    ax[0].scatter(X_train, y_train, color="red",           label="train")
+    ax[0].scatter(X_cv, y_cv,       color=dlc["dlorange"], label="cv")
+    ax[0].set_xlim(ax[0].get_xlim())
+    ax[0].set_ylim(ax[0].get_ylim())
+    for i in range(0,max_degree):
+        ax[0].plot(x, y_pred[:, i],  lw=0.5, label=f"{i+1}")
+    ax[0].legend(loc='upper left')
+
+    ax[1].set_title("error vs degree", fontsize=12)
+    cpts = list(range(1, max_degree+1))
+    ax[1].plot(cpts, err_train[0:], marker='o', label="train error", lw=2,  color=dlc["dlblue"])
+    ax[1].plot(cpts, err_cv[0:],    marker='o', label="cv error",  lw=2, color=dlc["dlorange"])
+    ax[1].set_ylim(*ax[1].get_ylim())
+    ax[1].axvline(optimal_degree, lw=1, color=dlc["dlmagenta"])
+    ax[1].annotate("optimal degree", xy=(optimal_degree, 80000), xycoords='data',
+                xytext=(0.3, 0.8), textcoords='axes fraction', fontsize=10,
+                   arrowprops=dict(arrowstyle="->", connectionstyle="arc3",
+                                   color=dlc['dldarkred'], lw=1))
+    ax[1].set_xlabel("degree")
+    ax[1].set_ylabel("error")
+    ax[1].legend()
+    fig.suptitle("Find Optimal Degree", fontsize=12)
+    plt.tight_layout()
+
+    plt.show()
