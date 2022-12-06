22e244b6084a3030a6555161c97512ce4c07f630
Fatih Karatay <fatihkaratay@gmail.com>
2022-12-06 16:26:23 -0500

    running k means algoritm

--- unsupervised_learning/clustering/k_means.py
+++ unsupervised_learning/clustering/k_means.py
@@ -3,6 +3,8 @@ This file contains some functionalities that we use in the K-Means clustering
 Algorithm
 """
 import numpy as np
+import matplotlib.pyplot as plt
+from utils import *
 
 
 def find_closest_centroids(X, centroids):
@@ -52,3 +54,36 @@ def compute_centroids(X, idx, K):
         centroids[i] = np.mean(points, axis=0)
 
     return centroids
+
+
+def run_kMeans(X, initial_centroids, max_iters=10, plot_progress=False):
+    """
+    Runs the K-Means algorithm on data matrix X, where each row of X
+    is a single example
+    """
+
+    # Initialize values
+    m, n = X.shape
+    K = initial_centroids.shape[0]
+    centroids = initial_centroids
+    previous_centroids = centroids
+    idx = np.zeros(m)
+
+    # Run K-Means
+    for i in range(max_iters):
+
+        # Output progress
+        print("K-Means iteration %d/%d" % (i, max_iters - 1))
+
+        # For each example in X, assign it to the closest centroid
+        idx = find_closest_centroids(X, centroids)
+
+        # Optionally plot progress
+        if plot_progress:
+            plot_progress_kMeans(X, centroids, previous_centroids, idx, K, i)
+            previous_centroids = centroids
+
+        # Given the memberships, compute new centroids
+        centroids = compute_centroids(X, idx, K)
+    plt.show()
+    return centroids, idx
