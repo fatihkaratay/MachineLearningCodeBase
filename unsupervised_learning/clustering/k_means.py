b50d48befb9b60f6b46053fc217321017213ebd9
Fatih Karatay <fatihkaratay@gmail.com>
2022-12-06 16:29:04 -0500

    initializing the k means

--- unsupervised_learning/clustering/k_means.py
+++ unsupervised_learning/clustering/k_means.py
@@ -87,3 +87,25 @@ def run_kMeans(X, initial_centroids, max_iters=10, plot_progress=False):
         centroids = compute_centroids(X, idx, K)
     plt.show()
     return centroids, idx
+
+
+def kMeans_init_centroids(X, K):
+    """
+    This function initializes K centroids that are to be
+    used in K-Means on the dataset X
+
+    Args:
+        X (ndarray): Data points
+        K (int):     number of centroids/clusters
+
+    Returns:
+        centroids (ndarray): Initialized centroids
+    """
+
+    # Randomly reorder the indices of examples
+    randidx = np.random.permutation(X.shape[0])
+
+    # Take the first K examples as centroids
+    centroids = X[randidx[:K]]
+
+    return centroids
