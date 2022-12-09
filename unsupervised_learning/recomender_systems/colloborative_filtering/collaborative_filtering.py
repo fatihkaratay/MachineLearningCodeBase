4dc41c03898e339ff9d748b0faf52d4709ed5805
Fatih Karatay <fatihkaratay@gmail.com>
2022-12-09 14:45:26 -0500

    cost function vectorized version is added.

--- unsupervised_learning/recomender_systems/colloborative_filtering/collaborative_filtering.py
+++ unsupervised_learning/recomender_systems/colloborative_filtering/collaborative_filtering.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 
 
 def cofi_cost_func(X, W, b, Y, R, lambda_):
@@ -27,4 +28,23 @@ def cofi_cost_func(X, W, b, Y, R, lambda_):
             J += np.square(r * (np.dot(w,x) + b_j - y ) )
     J = J/2 + (lambda_/2) * (np.sum(np.square(W)) + np.sum(np.square(X)))
 
+    return J
+
+
+def cofi_cost_func_vec(X, W, b, Y, R, lambda_):
+    """
+    Returns the cost for the content-based filtering
+    Vectorized for speed. Uses tensorflow operations to be compatible with custom training loop.
+    Args:
+      X (ndarray (num_movies,num_features)): matrix of item features
+      W (ndarray (num_users,num_features)) : matrix of user parameters
+      b (ndarray (1, num_users)            : vector of user parameters
+      Y (ndarray (num_movies,num_users)    : matrix of user ratings of movies
+      R (ndarray (num_movies,num_users)    : matrix, where R(i, j) = 1 if the i-th movies was rated by the j-th user
+      lambda_ (float): regularization parameter
+    Returns:
+      J (float) : Cost
+    """
+    j = (tf.linalg.matmul(X, tf.transpose(W)) + b - Y)*R
+    J = 0.5 * tf.reduce_sum(j**2) + (lambda_/2) * (tf.reduce_sum(X**2) + tf.reduce_sum(W**2))
     return J
