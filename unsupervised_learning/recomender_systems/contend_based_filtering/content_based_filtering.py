9e11ad3cc4e9f0b9e5780ed93c4b7ddb8a85f184
Fatih Karatay <fatihkaratay@gmail.com>
2022-12-12 09:22:59 -0500

    deep learning for content based learning is added.

---
+++ unsupervised_learning/recomender_systems/contend_based_filtering/content_based_filtering.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+
+item_train, user_train, y_train, item_features, user_features, item_vecs, movie_dict, user_to_genre = load_data()
+
+
+num_user_features = user_train.shape[1] - 3  # remove userid, rating count and ave rating during training
+num_item_features = item_train.shape[1] - 1  # remove movie id at train time
+
+num_outputs = 32
+tf.random.set_seed(1)
+user_NN = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(num_outputs),
+])
+
+item_NN = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(num_outputs),
+])
+
+# create the user input and point to the base network
+input_user = tf.keras.layers.Input(shape=(num_user_features))
+vu = user_NN(input_user)
+vu = tf.linalg.l2_normalize(vu, axis=1)
+
+# create the item input and point to the base network
+input_item = tf.keras.layers.Input(shape=(num_item_features))
+vm = item_NN(input_item)
+vm = tf.linalg.l2_normalize(vm, axis=1)
+
+# compute the dot product of the two vectors vu and vm
+output = tf.keras.layers.Dot(axes=1)([vu, vm])
+
+# specify the inputs and output of the model
+model = tf.keras.Model([input_user, input_item], output)
+
+model.summary()
